a7f85d38af0e8d7c2e3d6385feb6d867ccd7b578
Naomi <namez.ramirez@hotmail.com>
2024-05-30 16:57:25 -0600

    reviews and favorites model

---
+++ aa19-python-group-project/app/models/favorites.py
@@ -0,0 +1,14 @@
+from .db import db, environment, SCHEMA
+
+class Favorite(db.Model):
+ __tablename__ = 'favorites'
+
+if environment == "production":
+      __table_args__ = {'schema': SCHEMA}
+
+id = db.Column(db.Integer, primary_key=True)
+user_id = db.Column(db.Integer, db.ForeignKey('users.id', onDelete='CASCADE'), nullable=False)
+product_id = db.Column(db.Integer, db.ForeignKey('products.id', onDelete='CASCADE'), nullable=False)
+
+user = db.relationship('User', back_populates='favorites')
+product = db.relationship('Product', back_populates='favorites')
---
+++ aa19-python-group-project/app/models/reviews.py
@@ -0,0 +1,16 @@
+from .db import db, environment, SCHEMA
+
+class Review(db.Model):
+  __tablename__ = 'reviews'
+
+  if environment == "production":
+      __table_args__ = {'schema': SCHEMA}
+
+  id = db.Column(db.Integer, primary_key=True)
+  content = db.Column(db.String, nullable=False)
+  rating = db.Column(db.Integer ,nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id', onDelete='CASCADE'), nullable=False)
+  product_id = db.Column(db.Integer, db.ForeignKey('products.id', onDelete='CASCADE'), nullable=False)
+
+  user = db.relationship('User', back_populates='reviews')
+  product = db.relationship('Product', back_populates='reviews')
