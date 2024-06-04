5fe2463ff5215cf08bdce26c084eb2d022ef6784
Naomi <namez.ramirez@hotmail.com>
2024-06-04 16:13:54 -0600

    routes

--- aa19-python-group-project/app/api/favorites_routes.py
+++ aa19-python-group-project/app/api/favorites_routes.py
@@ -7,18 +7,3 @@ favorites_bp = Blueprint('favorites', __name__)
 def get_favorites():
     favorites = Favorite.query.all()
     return jsonify([fav.to_dict() for fav in favorites])
-
-@favorites_bp.route('/', methods=['POST'])
-def add_to_favorites():
-    data = request.get_json()
-    new_favorite = Favorite(**data)
-    db.session.add(new_favorite)
-    db.session.commit()
-    return jsonify(new_favorite.to_dict()), 201
-
-@favorites_bp.route('/<int:favorite_id>', methods=['DELETE'])
-def remove_from_favorites(favorite_id):
-    favorite = Favorite.query.get_or_404(favorite_id)
-    db.session.delete(favorite)
-    db.session.commit()
-    return '', 204
--- aa19-python-group-project/app/api/products_routes.py
+++ aa19-python-group-project/app/api/products_routes.py
@@ -7,27 +7,3 @@ products_bp = Blueprint('products', __name__)
 def get_products():
     products = Product.query.all()
     return jsonify([product.to_dict() for product in products])
-
-@products_bp.route('/', methods=['POST'])
-def create_product():
-    data = request.get_json()
-    new_product = Product(**data)
-    db.session.add(new_product)
-    db.session.commit()
-    return jsonify(new_product.to_dict()), 201
-
-@products_bp.route('/<int:product_id>', methods=['PUT'])
-def update_product(product_id):
-    data = request.get_json()
-    product = Product.query.get_or_404(product_id)
-    for key, value in data.items():
-        setattr(product, key, value)
-    db.session.commit()
-    return jsonify(product.to_dict())
-
-@products_bp.route('/<int:product_id>', methods=['DELETE'])
-def delete_product(product_id):
-    product = Product.query.get_or_404(product_id)
-    db.session.delete(product)
-    db.session.commit()
-    return '', 204
