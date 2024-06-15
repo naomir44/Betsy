8e6badbdb9a5bac308773c357d6c2d635ad9a4c7
samcovert13@gmail.com <samcovert13@gmail.com>
2024-06-15 14:52:07 -0600

    add to cart from favorites

--- react-vite/src/components/FavoritesPage/FavoritesPage.jsx
+++ react-vite/src/components/FavoritesPage/FavoritesPage.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchFavorites, fetchRemoveFavorite } from '../../redux/favorites';
 import './FavoritesPage.css';
+import { fetchAddCartItem } from '../../redux/cart-items';
 
 const FavoritesPage = () => {
   const dispatch = useDispatch();
   const favorites = useSelector(state => state.favorites.items);
+  const user = useSelector(state => state.session.user)
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -16,6 +18,17 @@ const FavoritesPage = () => {
     dispatch(fetchRemoveFavorite(productId));
   };
 
+  const handleAddToCart = async (productId) => {
+      const payload = {
+        user_id: user.id,
+        product_id: productId,
+        quantity: 1
+      }
+      const addedItem = await dispatch(fetchAddCartItem(payload))
+      if (addedItem) {
+        alert("Item added to your cart! Go to your cart to checkout!")
+      }
+  }
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -34,7 +47,7 @@ const FavoritesPage = () => {
               <p className='fav-description'>{product.description}</p>
               <p className='fav-price'>${product.price}</p>
               <div className='fav-actions'>
-                <button className='fav-add-to-cart'>Add to cart</button>
+                <button className='fav-add-to-cart' onClick={() => handleAddToCart(product.id)}>Add to cart</button>
                 <button className='fav-remove' onClick={() => handleRemoveFavorite(product.id)}>Remove</button>
               </div>
             </div>
